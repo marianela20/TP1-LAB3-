3313b92c9840ca99e2460964d2003218ac0136e7
Marianela Bustamante <bustamantemarianela18@gmail.com>
2023-05-03 00:00:03 -0300

    agregando archivo

---
+++ Globo.js
@@ -0,0 +1,49 @@
+
+function seguir(evento){
+    evento.preventDefault();
+    let seguir = document.getElementById('btnSeguir');
+    let texto = seguir.textContent;
+    if(texto == 'Seguir'){
+        seguir.textContent = 'Dejar de seguir';
+    }
+    else{
+        seguir.textContent = 'Seguir';
+    }
+    
+}
+document.getElementById('btnSeguir').addEventListener('click', seguir);
+
+
+function darMeGusta(evento){
+    evento.preventDefault();
+    
+    let likes = document.getElementById('likes');
+    likes.textContent = '201';
+
+}
+document.getElementById('meGusta').addEventListener('click', darMeGusta);
+
+
+function agregarComentario(evento){
+    evento.preventDefault();
+    let usuarioIngresado = document.getElementById('name').value;
+    let textoComentario = document.getElementById('comentario').value;
+    if(usuarioIngresado.trim() === ''){
+        alert('Ingrese su usuario');
+        return
+    }
+    else if(textoComentario.trim() === ''){
+        alert('Ingrese un comentario');
+        return
+    }
+
+    let texto = document.createTextNode(usuarioIngresado + ': ' + textoComentario);
+    let parrafoo = document.createElement('p')
+    parrafoo.appendChild(texto);
+    document.getElementById('listadoComentarios').appendChild(parrafoo);
+    
+    usuarioIngresado = document.getElementById('name').value = '';
+    textoComentario = document.getElementById('comentario').value = '';
+}
+document.getElementById('textoComentario').addEventListener('click',agregarComentario);
+
